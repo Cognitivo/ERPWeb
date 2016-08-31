4d260259a006b91a4e74e907475c2f4372e9d1a6
ehgarcia25 <eduardo@cognitivo.in>
2016-08-31 15:44:53 -0400

    update eduardo

--- app/Contact.php
+++ app/Contact.php
@@ -7,7 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {   
 	public $primaryKey='id_contact';
+
     protected $table='contacts';
+
     protected $fillable=['timestamp','name', 'gov_code'];
+    
     public $timestamps = false;
 }
