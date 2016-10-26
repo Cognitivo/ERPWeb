596d22a689cf555c1fa7cb7934782f6855464a10
Heti <heti@cognitivo.in>
2016-10-26 20:04:11 -0300

    update components on filter date

--- storage/app/SQL/AvgSalesPerInv.sql
+++ storage/app/SQL/AvgSalesPerInv.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 select round(ifnull(avg(salesperinvoice),0),2) as AvgSales
 from (
     select sum(sid.quantity * sid.unit_price) as salesperinvoice , si.id_sales_invoice
@@ -7,18 +7,3 @@ from (
         on si.id_sales_invoice = sid.id_sales_invoice
         where si.trans_date between @StartDate and @EndDate
         group by si.id_sales_invoice) as QuantityPerInvoice;
-=======
-                                                                                                                                                                                
-select round(ifnull(avg(salesperinvoice),0),2) as AvgSales
-from (select sum(sid.quantity*sid.unit_price)as salesperinvoice , si.id_sales_invoice
-from sales_invoice as si
-left join sales_invoice_detail as sid
-on si.id_sales_invoice = sid.id_sales_invoice
-where si.trans_date between @StartDate and @EndDate
-group by si.id_sales_invoice) as QuantityPerInvoice;
-
-                                            
-                                            
-                                            
-                                            
->>>>>>> a61bad64ead3280c28954e29bd2b414302e7c123
