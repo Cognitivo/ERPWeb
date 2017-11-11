f8c2a4f8e7332f866f7957581f984ee32977e9e1
pankeel-cognitivo <pankeel@cognitivo.in>
2017-11-11 15:35:59 +0530

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,7 +9,7 @@
 | and give it the controller to call when that URI is requested.
 |
  */
- Route::group(['middleware' => 'auth','cors'], function () {
+ Route::group(['middleware' => 'cors'], function () {
  Route::get('api/login/{username}/{password}', 'Auth\AuthController@Login');
 });
 Route::get('auth/login', 'Auth\AuthController@getLogin');
