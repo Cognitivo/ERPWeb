d82b9b4301917b080ff9d8f1ef7e83057d4451df
pankeel-cognitivo <pankeel@cognitivo.in>
2017-11-21 16:55:44 +0530

    changes

--- app/Http/Controllers/Production/ProductionOrderDetailController.php
+++ app/Http/Controllers/Production/ProductionOrderDetailController.php
@@ -143,7 +143,7 @@ class ProductionOrderDetailController extends Controller
     }
     public function showdetail($id)
     {
-        $ProductionOrderDetail = ProductionOrderDetail::where('id_order_detail','=',$id)->first();
+        $ProductionOrderDetail = ProductionOrderDetail::where('id_order_detail','=',$id)->get();
 
 
         return response()->json($ProductionOrderDetail);
