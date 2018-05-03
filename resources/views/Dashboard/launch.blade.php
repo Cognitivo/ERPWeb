78c136f48a2f27910f052cdb1f00e2f2c84fa0d5
abhi-cognitivo <abhi@cognitivo.in>
2018-05-03 12:13:05 -0400

    update

--- resources/views/Dashboard/launch.blade.php
+++ resources/views/Dashboard/launch.blade.php
@@ -2,6 +2,7 @@
 
 @section('title', 'Dashboard | CognitivoERP')
 @section('Title', 'Dashboard')
+
 @section('pagesettings')
 	<div class="col-md-4">
 		<div id="reportrange" class="btn default">
@@ -11,12 +12,48 @@
 		</div>
 	</div>
 @endsection
+
 @section('innercontent')
-	<div class="col-md-4 ">
-		<div class="portlet">
-			<div class="portlet-title">
-				<div class="caption">
-					<i class="fa fa-gift"></i>Quantity Per Customer </div>
+	<div class="row">
+		<div class="col-md-6">
+			<div class="portlet">
+				<div class="portlet-title">
+					<div class="caption">
+						<i class="fa fa-gift"></i>Cantidad Vendida por Cliente
+					</div>
+					<div class="tools">
+						<a href="javascript:;" class="collapse" data-original-title="" title=""> </a>
+						<a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title=""> </a>
+						<a href="" class="fullscreen" data-original-title="" title=""> </a>
+						<a href="javascript:;" class="reload" data-original-title="" title=""> </a>
+					</div>
+				</div>
+				<div class="portlet-body" >
+					<table class="table table-condensed" id="table-production-execution-form">
+						<thead>
+							<tr>
+								<th>Cliente</th>
+								<th>Cantidad</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach ($quantitypercustomer as $value)
+								<tr>
+									<td>{{ $value->contact }}</td>
+									<td>{{ $value->quantity }}</td>
+								</tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div class="portlet">
+				<div class="portlet-title">
+					<div class="caption">
+						<i class="fa fa-gift"></i>Valor Vendido por Cliente
+					</div>
 					<div class="tools">
 						<a href="javascript:;" class="collapse" data-original-title="" title=""> </a>
 						<a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title=""> </a>
@@ -30,14 +67,13 @@
 							<tr>
 								<th>Contact</th>
 								<th>Quantity</th>
-
 							</tr>
 						</thead>
 						<tbody>
-							@foreach ($quantitypercustomer as $key => $value)
+							@foreach ($salespercustomer as $value)
 								<tr>
 									<td>{{ $value->contact }}</td>
-									<td>{{ $value->quantity }}</td>
+									<td>{{ $value->sales }}</td>
 								</tr>
 							@endforeach
 						</tbody>
@@ -46,42 +82,15 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-md-6" id="barpieportlet" >
-			<div class="portlet">
-				<div class="portlet-title">
-					<div class="caption">
-						<i class="fa fa-gift"></i>Sales Per Customer </div>
-						<div class="tools">
-							<a href="javascript:;" class="collapse" data-original-title="" title=""> </a>
-							<a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title=""> </a>
-							<a href="" class="fullscreen" data-original-title="" title=""> </a>
-							<a href="javascript:;" class="reload" data-original-title="" title=""> </a>
-						</div>
-					</div>
-					<div class="portlet-body" >
-						<table class="table table-condensed" id="table-production-execution-form">
-							<thead>
-								<tr>
-									<th>Contact</th>
-									<th>Quantity</th>
+	</div>
+	<div class="row">
+		<div class="" id="barpieportlet">
 
-								</tr>
-							</thead>
-							<tbody>
-								@foreach ($salespercustomer as $key => $value)
-									<tr>
-										<td>{{ $value->contact }}</td>
-										<td>{{ $value->sales }}</td>
-									</tr>
-								@endforeach
-							</tbody>
-						</table>
+		</div>
+	</div>
+@endsection
 
-					</div>
-				</div>
-			</div>
-	@endsection
-	@section('pagescripts')
-		@parent
-		<script src="{{url()}}/assets/pages/scripts/add-dashboard-components.js" type="text/javascript"></script>
-	@endsection
+@section('pagescripts')
+	@parent
+	<script src="{{url()}}/assets/pages/scripts/add-dashboard-components.js" type="text/javascript"></script>
+@endsection
