421c76754ac1cb1d102dfc62fdb1daae1b10e064
Pankeel <pankeel@cognitivo.in>
2018-05-03 17:20:54 +0530

    update

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -17,7 +17,42 @@ use App\Http\Controllers\ComponentController;
 class dashboardController extends Controller
 {
   public function index(Request $request){
-      return view('Dashboard.launch');
+
+    $salespercustomer = DB::select('
+    select contacts.name as contact,
+    round(ifnull(sum(quantity * unit_price * vatco.coef),0),2) as sales
+    from sales_invoice as si
+    left join sales_invoice_detail as sd
+    on si.id_sales_invoice = sd.id_sales_invoice
+    left join(select app_vat_group.id_vat_group, sum(app_vat.coefficient) + 1 as coef
+              from app_vat_group
+              left join app_vat_group_details
+              on app_vat_group.id_vat_group = app_vat_group_details.id_vat_group
+              left join app_vat
+              on app_vat_group_details.id_vat = app_vat.id_vat
+              group by app_vat_group.id_vat_group) as vatco
+    on sd.id_vat_group = vatco.id_vat_group
+    left join contacts
+    on contacts.id_contact = si.id_contact
+    group by  si.id_contact');
+
+        $salespercustomer = collect($salespercustomer);
+
+        $quantitypercustomer = DB::select('
+        select contacts.name as contact,
+        round(sum(quantity)) as quantity
+        from sales_invoice as si
+        left join sales_invoice_detail as sd
+        on si.id_sales_invoice = sd.id_sales_invoice
+        left join contacts
+        on contacts.id_contact = si.id_contact
+        group by  contacts.id_contact');
+
+            $quantitypercustomer = collect($quantitypercustomer);
+
+      return view('Dashboard.launch')
+      ->with('quantitypercustomer',$quantitypercustomer)
+      ->with('salespercustomer',$salespercustomer);
   }
   public function SaveDashboard(Request $request){
     $Name = Auth::user()->name;
--- resources/views/Dashboard/launch.blade.php
+++ resources/views/Dashboard/launch.blade.php
@@ -3,33 +3,85 @@
 @section('title', 'Dashboard | CognitivoERP')
 @section('Title', 'Dashboard')
 @section('pagesettings')
-<div class="col-md-4">
-    <div id="reportrange" class="btn default">
-        <i class="fa fa-calendar"></i> &nbsp;
-        <span></span>
-        <b class="fa fa-angle-down"></b>
-    </div>
-</div>
+	<div class="col-md-4">
+		<div id="reportrange" class="btn default">
+			<i class="fa fa-calendar"></i> &nbsp;
+			<span></span>
+			<b class="fa fa-angle-down"></b>
+		</div>
+	</div>
 @endsection
 @section('innercontent')
-<div class="col-md-6" id="barpieportlet" style="display:none;">                       
-	<div class="portlet">
-	    <div class="portlet-title">
-	        <div class="caption">
-	            <i class="fa fa-gift"></i>Portlet </div>
-	        <div class="tools">
-	            <a href="javascript:;" class="collapse" data-original-title="" title=""> </a>
-	            <a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title=""> </a>
-	            <a href="" class="fullscreen" data-original-title="" title=""> </a>
-	            <a href="javascript:;" class="reload" data-original-title="" title=""> </a>
-	        </div>
-	    </div>
-	    <div class="portlet-body" style="display: block;">  </div>
-	</div>
-</div>
-@endsection
-@section('pagescripts')
-@parent
-<script src="{{url()}}/assets/pages/scripts/add-dashboard-components.js" type="text/javascript"></script>
-@endsection
+	<div class="col-md-4 ">
+		<div class="portlet">
+			<div class="portlet-title">
+				<div class="caption">
+					<i class="fa fa-gift"></i>Quantity Per Customer </div>
+					<div class="tools">
+						<a href="javascript:;" class="collapse" data-original-title="" title=""> </a>
+						<a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title=""> </a>
+						<a href="" class="fullscreen" data-original-title="" title=""> </a>
+						<a href="javascript:;" class="reload" data-original-title="" title=""> </a>
+					</div>
+				</div>
+				<div class="portlet-body" >
+					<table class="table table-condensed" id="table-production-execution-form">
+						<thead>
+							<tr>
+								<th>Contact</th>
+								<th>Quantity</th>
 
+							</tr>
+						</thead>
+						<tbody>
+							@foreach ($quantitypercustomer as $key => $value)
+								<tr>
+									<td>{{ $value->contact }}</td>
+									<td>{{ $value->quantity }}</td>
+								</tr>
+							@endforeach
+						</tbody>
+					</table>
+
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6" id="barpieportlet" >
+			<div class="portlet">
+				<div class="portlet-title">
+					<div class="caption">
+						<i class="fa fa-gift"></i>Sales Per Customer </div>
+						<div class="tools">
+							<a href="javascript:;" class="collapse" data-original-title="" title=""> </a>
+							<a href="#portlet-config" data-toggle="modal" class="config" data-original-title="" title=""> </a>
+							<a href="" class="fullscreen" data-original-title="" title=""> </a>
+							<a href="javascript:;" class="reload" data-original-title="" title=""> </a>
+						</div>
+					</div>
+					<div class="portlet-body" >
+						<table class="table table-condensed" id="table-production-execution-form">
+							<thead>
+								<tr>
+									<th>Contact</th>
+									<th>Quantity</th>
+
+								</tr>
+							</thead>
+							<tbody>
+								@foreach ($salespercustomer as $key => $value)
+									<tr>
+										<td>{{ $value->contact }}</td>
+										<td>{{ $value->sales }}</td>
+									</tr>
+								@endforeach
+							</tbody>
+						</table>
+
+					</div>
+				</div>
+			</div>
+	@endsection
+	@section('pagescripts')
+		@parent
+		<script src="{{url()}}/assets/pages/scripts/add-dashboard-components.js" type="text/javascript"></script>
+	@endsection
