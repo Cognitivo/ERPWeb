448b206c213c5c7df4f980845a4f8f68066dcbd4
abhi-cognitivo <abhi@cognitivo.in>
2018-05-04 14:42:58 -0400

    up

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -16,25 +16,25 @@ use App\Http\Controllers\ComponentController;
 
 class dashboardController extends Controller
 {
-  public function index(Request $request){
+    public function index(Request $request){
 
-    $salespercustomer = DB::select('
-    select contacts.name as contact,
-    round(ifnull(sum(quantity * unit_price * vatco.coef),0),2) as sales
-    from sales_invoice as si
-    left join sales_invoice_detail as sd
-    on si.id_sales_invoice = sd.id_sales_invoice
-    left join(select app_vat_group.id_vat_group, sum(app_vat.coefficient) + 1 as coef
-              from app_vat_group
-              left join app_vat_group_details
-              on app_vat_group.id_vat_group = app_vat_group_details.id_vat_group
-              left join app_vat
-              on app_vat_group_details.id_vat = app_vat.id_vat
-              group by app_vat_group.id_vat_group) as vatco
-    on sd.id_vat_group = vatco.id_vat_group
-    left join contacts
-    on contacts.id_contact = si.id_contact
-    group by  si.id_contact');
+        $salespercustomer = DB::select('
+        select contacts.name as contact,
+        round(ifnull(sum(quantity * unit_price * vatco.coef),0),2) as sales
+        from sales_invoice as si
+        left join sales_invoice_detail as sd
+        on si.id_sales_invoice = sd.id_sales_invoice
+        left join(select app_vat_group.id_vat_group, sum(app_vat.coefficient) + 1 as coef
+        from app_vat_group
+        left join app_vat_group_details
+        on app_vat_group.id_vat_group = app_vat_group_details.id_vat_group
+        left join app_vat
+        on app_vat_group_details.id_vat = app_vat.id_vat
+        group by app_vat_group.id_vat_group) as vatco
+        on sd.id_vat_group = vatco.id_vat_group
+        left join contacts
+        on contacts.id_contact = si.id_contact
+        group by  si.id_contact');
 
         $salespercustomer = collect($salespercustomer);
 
@@ -48,30 +48,37 @@ class dashboardController extends Controller
         on contacts.id_contact = si.id_contact
         group by  contacts.id_contact');
 
-            $quantitypercustomer = collect($quantitypercustomer);
+        $quantitypercustomer = collect($quantitypercustomer);
 
-      return view('Dashboard.launch')
-      ->with('quantitypercustomer',$quantitypercustomer)
-      ->with('salespercustomer',$salespercustomer);
-  }
-  public function SaveDashboard(Request $request){
-    $Name = Auth::user()->name;
-    if (!file_exists(Config::get("Paths.UserDashboard") . $Name . "/")) {
-      File::makeDirectory(Config::get("Paths.UserDashboard") . $Name . "/");
+        return view('Dashboard.launch')
+        ->with('quantitypercustomer',$quantitypercustomer)
+        ->with('salespercustomer',$salespercustomer);
     }
-    try{
-      foreach (Input::get('components') as $Comp) {
-        $DashboardComponents[] = $Comp;
-      }
-      file_put_contents(Config::get("Paths.UserDashboard") . $Name . "/dashboard.json",json_encode($DashboardComponents));
+
+    public function SaveDashboard(Request $request)
+    {
+        $Name = Auth::user()->name;
+        if (!file_exists(Config::get("Paths.UserDashboard") . $Name . "/")) {
+            File::makeDirectory(Config::get("Paths.UserDashboard") . $Name . "/");
+        }
+        try
+        {
+            foreach (Input::get('components') as $Comp)
+            {
+                $DashboardComponents[] = $Comp;
+            }
+            file_put_contents(Config::get("Paths.UserDashboard") . $Name . "/dashboard.json",json_encode($DashboardComponents));
+        }
+        catch(Exception $e)
+        {
+            return $e->getMessage();
+        }
+
+        return redirect('/');
     }
-    catch(Exception $e){
-      return $e->getMessage();
+
+    public function ManageDashboard(){
+        $Components = (new ComponentController)->ManageComponents();
+        return view('Dashboard.ConfigComponents')->with('Components',$Components);
     }
-    return redirect('/');
-  }
-  public function ManageDashboard(){
-    $Components = (new ComponentController)->ManageComponents();
-    return view('Dashboard.ConfigComponents')->with('Components',$Components);
-  }
 }
--- config/Paths.php
+++ config/Paths.php
@@ -1,7 +1,7 @@
 <?php
   return[
-    'Components' => storage_path() . '/app/config/Components/',
+    'Components' => storage_path() . '/app/Config/Components/',
     'SQLs' => storage_path() . '/app/SQL/',
-    'UserDashboard' => storage_path() . '/app/config/users/'
+    'UserDashboard' => storage_path() . '/app/Config/users/'
   ];
  ?>
